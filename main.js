07e640392329b84caf00c9303475ba7faf4076bb
psydie <83035548+psydie@users.noreply.github.com>
2022-11-26 09:33:05 +0100

    Save Test

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 "use strict";
-
-/*
- * Created with @iobroker/create-adapter v2.3.0
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
-
-/**
- * The adapter instance
- * @type {ioBroker.Adapter}
- */
+const axios = require('axios');
+const fs = require('fs');
+const tools = require('./lib/tools');
+/** @type {number | undefined}*/
+let timerRequest;
+/** @type {ioBroker.Adapter} */
 let adapter;
 
 /**
@@ -22,8 +13,6 @@ let adapter;
  * @param {Partial<utils.AdapterOptions>} [options]
  */
 function startAdapter(options) {
-	// Create the adapter and define its methods
-	return adapter = utils.adapter(Object.assign({}, options, {
 		name: "tasmoupdate",
 
 		// The ready callback is called when databases are connected and adapter received configuration.
@@ -33,29 +22,13 @@ function startAdapter(options) {
 		// is called when adapter shuts down - callback has to be called under any circumstances!
 		unload: (callback) => {
 			try {
-				// Here you must clear all timeouts or intervals that may still be active
-				// clearTimeout(timeout1);
-				// clearTimeout(timeout2);
-				// ...
-				// clearInterval(interval1);
-
+				adapter.log.debug('cleaned everything up...');
+				clearTimeout(timerRequest);
 				callback();
 			} catch (e) {
 				callback();
 			}
 		},
-
-		// If you need to react to object changes, uncomment the following method.
-		// You also need to subscribe to the objects with `adapter.subscribeObjects`, similar to `adapter.subscribeStates`.
-		// objectChange: (id, obj) => {
-		// 	if (obj) {
-		// 		// The object was changed
-		// 		adapter.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-		// 	} else {
-		// 		// The object was deleted
-		// 		adapter.log.info(`object ${id} deleted`);
-		// 	}
-		// },
 
 		// is called if a subscribed state changes
 		stateChange: (id, state) => {
@@ -68,30 +41,9 @@ function startAdapter(options) {
 			}
 		},
 
-		// If you need to accept messages in your adapter, uncomment the following block.
-		// /**
-		//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-		//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-		//  */
-		// message: (obj) => {
-		// 	if (typeof obj === "object" && obj.message) {
-		// 		if (obj.command === "send") {
-		// 			// e.g. send email or pushover or whatever
-		// 			adapter.log.info("send command");
-
-		// 			// Send response in callback if required
-		// 			if (obj.callback) adapter.sendTo(obj.from, obj.command, "Message received", obj.callback);
-		// 		}
-		// 	}
-		// },
 	}));
 }
-
-async function main() {
-
-	// Reset the connection indicator during startup
-	await adapter.setStateAsync("info.connection", false, true);
-
+function adapterlog{
 	// The adapters config (in the instance object everything under the attribute "native") is accessible via
 	// adapter.config:
 	adapter.log.info("config ota_de: " + adapter.config.ota_de);
@@ -117,12 +69,106 @@ async function main() {
 	adapter.log.info("config setoption16: " + adapter.config.setoption16);
 	adapter.log.info("config setoption17: " + adapter.config.setoption17);
 	adapter.log.info("config setoption18: " + adapter.config.setoption18);
-
-	/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-	*/
+	adapter.log.info("config setoption19: " + adapter.config.setoption19);
+	adapter.log.info("config setoption20: " + adapter.config.setoption20);
+	adapter.log.info("config setoption21: " + adapter.config.setoption21);
+	adapter.log.info("config setoption24: " + adapter.config.setoption24);
+	adapter.log.info("config setoption26: " + adapter.config.setoption26);
+	adapter.log.info("config setoption28: " + adapter.config.setoption28);
+	adapter.log.info("config setoption29: " + adapter.config.setoption29);
+	adapter.log.info("config setoption30: " + adapter.config.setoption30);
+	adapter.log.info("config setoption31: " + adapter.config.setoption31);
+	adapter.log.info("config setoption32: " + adapter.config.setoption32);
+	adapter.log.info("config setoption33: " + adapter.config.setoption33);
+	adapter.log.info("config setoption34: " + adapter.config.setoption34);
+	adapter.log.info("config setoption36: " + adapter.config.setoption36);
+	adapter.log.info("config setoption38: " + adapter.config.setoption38);
+	adapter.log.info("config setoption39: " + adapter.config.setoption39);
+	adapter.log.info("config setoption40: " + adapter.config.setoption40);
+	adapter.log.info("config setoption41: " + adapter.config.setoption41);
+	adapter.log.info("config setoption42: " + adapter.config.setoption42);
+	adapter.log.info("config setoption43: " + adapter.config.setoption43);
+	adapter.log.info("config setoption44: " + adapter.config.setoption44);
+	adapter.log.info("config setoption45: " + adapter.config.setoption45);
+	adapter.log.info("config setoption46: " + adapter.config.setoption46);
+	adapter.log.info("config setoption51: " + adapter.config.setoption51);
+	adapter.log.info("config setoption52: " + adapter.config.setoption52);
+	adapter.log.info("config setoption53: " + adapter.config.setoption53);
+	adapter.log.info("config setoption54: " + adapter.config.setoption54);
+	adapter.log.info("config setoption55: " + adapter.config.setoption55);
+	adapter.log.info("config setoption56: " + adapter.config.setoption56);
+	adapter.log.info("config setoption57: " + adapter.config.setoption57);
+	adapter.log.info("config setoption58: " + adapter.config.setoption58);
+	adapter.log.info("config setoption59: " + adapter.config.setoption59);
+	adapter.log.info("config setoption60: " + adapter.config.setoption60);
+	adapter.log.info("config setoption61: " + adapter.config.setoption61);
+	adapter.log.info("config setoption62: " + adapter.config.setoption62);
+	adapter.log.info("config setoption63: " + adapter.config.setoption63);
+	adapter.log.info("config setoption64: " + adapter.config.setoption64);
+	adapter.log.info("config setoption65: " + adapter.config.setoption65);
+	adapter.log.info("config setoption66: " + adapter.config.setoption66);
+	adapter.log.info("config setoption69: " + adapter.config.setoption69);
+	adapter.log.info("config setoption71: " + adapter.config.setoption71);
+	adapter.log.info("config setoption72: " + adapter.config.setoption72);
+	adapter.log.info("config setoption73: " + adapter.config.setoption73);
+	adapter.log.info("config setoption74: " + adapter.config.setoption74);
+	adapter.log.info("config setoption75: " + adapter.config.setoption75);
+	adapter.log.info("config setoption76: " + adapter.config.setoption76);
+	adapter.log.info("config setoption77: " + adapter.config.setoption77);
+	adapter.log.info("config setoption78: " + adapter.config.setoption78);
+	adapter.log.info("config setoption79: " + adapter.config.setoption79);
+	adapter.log.info("config setoption80: " + adapter.config.setoption80);
+	adapter.log.info("config setoption81: " + adapter.config.setoption81);
+	adapter.log.info("config setoption82: " + adapter.config.setoption82);
+	adapter.log.info("config setoption83: " + adapter.config.setoption83);
+	adapter.log.info("config setoption84: " + adapter.config.setoption84);
+	adapter.log.info("config setoption85: " + adapter.config.setoption85);
+	adapter.log.info("config setoption86: " + adapter.config.setoption86);
+	adapter.log.info("config setoption87: " + adapter.config.setoption87);
+	adapter.log.info("config setoption88: " + adapter.config.setoption88);
+	adapter.log.info("config setoption90: " + adapter.config.setoption90);
+	adapter.log.info("config setoption93: " + adapter.config.setoption93);
+	adapter.log.info("config setoption94: " + adapter.config.setoption94);
+	adapter.log.info("config setoption97: " + adapter.config.setoption97);
+	adapter.log.info("config setoption98: " + adapter.config.setoption98);
+	adapter.log.info("config setoption99: " + adapter.config.setoption99);
+	adapter.log.info("config setoption101: " + adapter.config.setoption101);
+	adapter.log.info("config setoption103: " + adapter.config.setoption103);
+	adapter.log.info("config setoption104: " + adapter.config.setoption104);
+	adapter.log.info("config setoption107: " + adapter.config.setoption107);
+	adapter.log.info("config setoption108: " + adapter.config.setoption108);
+	adapter.log.info("config setoption109: " + adapter.config.setoption109);
+	adapter.log.info("config setoption113: " + adapter.config.setoption113);
+	adapter.log.info("config setoption114: " + adapter.config.setoption114);
+	adapter.log.info("config setoption115: " + adapter.config.setoption115);
+	adapter.log.info("config setoption116: " + adapter.config.setoption116);
+	adapter.log.info("config setoption117: " + adapter.config.setoption117);
+	adapter.log.info("config setoption123: " + adapter.config.setoption123);
+	adapter.log.info("config setoption124: " + adapter.config.setoption124);
+	adapter.log.info("config setoption125: " + adapter.config.setoption125);
+	adapter.log.info("config setoption126: " + adapter.config.setoption126);
+	adapter.log.info("config setoption127: " + adapter.config.setoption127);
+	adapter.log.info("config setoption128: " + adapter.config.setoption128);
+	adapter.log.info("config setoption129: " + adapter.config.setoption129);
+	adapter.log.info("config setoption130: " + adapter.config.setoption130);
+	adapter.log.info("config setoption131: " + adapter.config.setoption131);
+	adapter.log.info("config setoption132: " + adapter.config.setoption132);
+	adapter.log.info("config setoption134: " + adapter.config.setoption134);
+	adapter.log.info("config setoption135: " + adapter.config.setoption135);
+	adapter.log.info("config setoption136: " + adapter.config.setoption136);
+	adapter.log.info("config setoption137: " + adapter.config.setoption137);
+	adapter.log.info("config setoption138: " + adapter.config.setoption138);
+	adapter.log.info("config setoption139: " + adapter.config.setoption139);
+	adapter.log.info("config setoption140: " + adapter.config.setoption140);
+	adapter.log.info("config setoption141: " + adapter.config.setoption141);
+	adapter.log.info("config setoption142: " + adapter.config.setoption142);
+	adapter.log.info("config setoption143: " + adapter.config.setoption143);
+	adapter.log.info("config setoption144: " + adapter.config.setoption144);
+	adapter.log.info("config setoption146: " + adapter.config.setoption146);
+	adapter.log.info("config setoption147: " + adapter.config.setoption147);
+	adapter.log.info("config setoption148: " + adapter.config.setoption148);
+}
+function createobjects{
 	await adapter.setObjectNotExistsAsync("testVariable", {
 		type: "state",
 		common: {
@@ -134,13 +180,103 @@ async function main() {
 		},
 		native: {},
 	});
-
-	// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+}
+function allsubscripe{
 	adapter.subscribeStates("testVariable");
-	// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-	// adapter.subscribeStates("lights.*");
-	// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-	// adapter.subscribeStates("*");
+}
+async function main() {
+	adapterlog();
+	// Reset the connection indicator during startup
+	await adapter.setStateAsync("info.connection", false, true);
+	createobjects();
+	allsubscripe();
+	await adapter.setStateAsync("info.connection", true, true);
+		//** Code zum Sortieren* /
+	var posKomma, ListeSonoffs, Schleifenende, IPAdress;
+
+
+	createState("javascript.0.Tasmota.Grundeinstellungen_setzen", false, async function () {
+	});
+	on({ id: "javascript.0.Tasmota.Grundeinstellungen_setzen"/*javascript.0.Tasmota.Grundeinstellungen_setzen*/, val: true }, async function (obj) {
+		var value = obj.state.val;
+		var oldValue = obj.oldState.val;
+		// Geäteliste auslesen und nach Komma separienen
+		posKomma = 0;
+		ListeSonoffs = String(getState("sonoff.0.info.connection").val) + ',';
+		Schleifenende = false;
+		while (Schleifenende == false) {
+			// Suche nächstes Komma in der Sonoff-Liste
+			posKomma = ListeSonoffs.indexOf(',') + 1;
+			if (posKomma == 0) {
+				// kein Komma mehr gefunden
+				Schleifenende = true;
+			} else {
+				// bis zum Zeichen vor dem Komma kopieren
+				// IP Nummer des Tasmotas auslesen
+				IPAdress = ['sonoff.0.', ListeSonoffs.slice(0, parseFloat(posKomma) - 1), '.INFO.Info2_IPAddress'].join('');
+				ListeSonoffs = ListeSonoffs.slice(((parseFloat(posKomma) + 1) - 1), ListeSonoffs.length);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=Setoption26 0'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=Setoption26 0'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=Setoption56 1'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=Setoption56 1'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=latitude ', getState("javascript.0.Tasmota.versionstyp.latitude").val].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=latitude ', getState("javascript.0.Tasmota.versionstyp.latitude").val].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=longitude ', getState("javascript.0.Tasmota.versionstyp.longitude").val].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=longitude ', getState("javascript.0.Tasmota.versionstyp.longitude").val].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=timezone 99'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=timezone 99'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=ledstate'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=ledstate'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=Setoption106'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=Setoption106'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=Setoption20'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=Setoption20'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=restart'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=restart'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=Otaurl'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=Otaurl'].join('')));
+				await wait(200);
+				try {
+					require("request")((['http://', getState(IPAdress).val, '/cm?cmnd=Upgrade'].join(''))).on("error", function (e) { console.error(e); });
+				} catch (e) { console.error(e); }
+				console.log("request: " + (['http://', getState(IPAdress).val, '/cm?cmnd=Upgrade'].join('')));
+				// Logdatei erstellen
+				console.log((String(getState(IPAdress).val) + 'wurde auf Tasmota eingestellt'));
+			}
+		}
+		setStateDelayed("javascript.0.Tasmota.Grundeinstellungen_setzen"/*javascript.0.Tasmota.Grundeinstellungen_setzen*/, false, true, 20000, false);
+	});
+
+
 
 	/*
 		setState examples
